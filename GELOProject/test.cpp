bfca87fd5f929f2d6e13879c434e172eb02e85c7
Lovishxd <112024075+Lovishxd@users.noreply.github.com>
2023-11-13 00:21:33 -0600

    2

--- GELOProject/test.cpp
+++ GELOProject/test.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <cstdlib>
-#include <cctype>
 #include <fstream>
+#include <chrono>
+#include <ctime>
 
-const double COFFEE_PRICE = 1.20;
-const double CHEESECAKE_PRICE = 2.00;
-const double COFFEE_BEANS_PRICE = 5.00;
+const double COFFEE_PRICE = 1.50;
+const double LARGE_COFFEE_PRICE = 1.90;
+const double CHEESECAKE_PRICE = 3.00;
+const double COFFEE_BEANS_PRICE = 0.60;
 const int MANAGER_PASSWORD = 1234;
 
+static int transactionIDCounter;
+
 using namespace std;
+
+// Function declarations
 void main_menu();
 int second_menu(int);
 double sellCoffee(int&);
@@ -21,6 +26,8 @@ void orderReview(double, double, double);
 void displayItemQuantities(int, int, double);
 void displayTotalCoffeeSold(double);
 void displayTotalMoneyMade(double);
+void recordTransaction(const string& items, int quantity, double total);
+void displaySalesData();
 
 int main() {
     int choice, choice2;
@@ -31,7 +38,7 @@ int main() {
     do {
         cout << "\n";
         main_menu();
-        cout << "\tPlease enter any of the above options. For example, enter 1 to order Coffee: ";
+        cout << "\tPlease enter the number corresponding to your choice: ";
         cin >> choice;
 
         switch (choice) {
@@ -46,15 +53,15 @@ int main() {
                 break;
             case 4:
                 orderReview(total1, total2, total3);
-                cout << "Your total including tax is: $" << fixed << setprecision(2) << (total1 + total2 + total3) * 1.7 << endl;
+                cout << "Your total including tax is: $" << fixed << setprecision(2) << (total1 + total2 + total3) + ((total1 + total2 + total3) * 0.07) << endl;
                 break;
             case 5:
-                cout << "You chose Option 5. \n";
-                cout << "What can I help you with the following options? Or you can call an associate by entering 3\n";
-                cout << "\t\t\t1. Manager Login.\n";
-                cout << "\t\t\t2. Employee Clock-in/out\n";
-                cout << "\t\t\t3. Call Assistance \a\n";
-                cout << "\t\t\t4. EXIT \a\n";
+                cout << "You chose Option 5. What can I help you with?\n";
+                cout << "\t1. Manager Login.\n";
+                cout << "\t2. Employee Clock-in/out\n";
+                cout << "\t3. Call Assistance\n";
+                cout << "\t4. EXIT\n";
+                cout << "Enter the number corresponding to your choice: ";
                 cin >> choice2;
                 if (choice2 == 1) {
                     second_menu(choice2);
@@ -67,15 +74,16 @@ int main() {
                 }
                 break;
             case 6:
-                cout << "\tThank you for shopping with us. Your order should be ready within 20 minutes \n";
+                recordTransaction("Coffee, Cheesecake, Coffee Beans", coffeeCups + cheesecakeSlices + coffeeBeansAmount, totalMoneyMade);
+                cout << "\tThank you for shopping with us. Your order should be ready within 20 minutes.\n";
                 exit(0);
                 break;
             default:
-                cout << "Invalid Choice. Please Retry";
+                cout << "Invalid Choice. Please retry.\n";
         }
 
         totalCoffeeSold = total1 + total2 + total3;
-        totalMoneyMade = (total1 + total2 + total3) * 1.7;
+        totalMoneyMade = (total1 + total2 + total3) + ((total1 + total2 + total3) * 0.07);
 
     } while (choice != 5 && choice != 6);
 
@@ -83,30 +91,29 @@ int main() {
 }
 
 void main_menu() {
-	
-	cout << "\t\t\t\t\t--------------------------------------------\n";
-	cout << "\t\t\t\t\t\tWelcome to Jason's Coffee Shop\t\n";
-	cout << "\t\t\t\t\t--------------------------------------------\n";
-	cout << "\tWhat would you like to buy today?\n";
-	cout << "\tOur Menu options are: \n";
-	cout << "\t\t\t1. Coffee \n";
-	cout << "\t\t\t2. Cheesecake \n";
-	cout << "\t\t\t3. Coffee Beans \n";
-	cout << "\t\t\t4. Review Order \n";
-	cout << "\t\t\t5. Help \n";
-	cout << "\t\t\t6. Complete \n";
-	cout << "\tPlease enter any of the above option for Example Enter 1 to order Coffee \n\n";
-	cout << "\tEnter 6 when you are done ordering\n\n\n";
-	
-	
-
+    auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    string currentTime = ctime(&now);
+    cout << "\t\t\t\t\t--------------------------------------------\n";
+    cout << "\t\t\t\t\t\tWelcome to Jason's Coffee Shop\t" << "\t\t\t\t\t\t" << currentTime << "\n";
+    cout << "\t\t\t\t\t--------------------------------------------\n";
+    cout << "\tWhat would you like to buy today?\n";
+    cout << "\tOur Menu options are: \n";
+    cout << "\t1. Coffee\n";
+    cout << "\t2. Cheesecake\n";
+    cout << "\t3. Coffee Beans\n";
+    cout << "\t4. Review Order\n";
+    cout << "\t5. Help\n";
+    cout << "\t6. Complete\n";
+    cout << "Enter the number corresponding to your choice.\n\n";
 }
+
 void managerActions(double total1, double total2, double total3, int coffeeCups, int cheesecakeSlices, double coffeeBeansAmount) {
     double totalCoffeeSold = total1 + total2 + total3;
-    double totalMoneyMade = (total1 + total2 + total3) * 1.7;
+    double totalMoneyMade = (total1 + total2 + total3) + ((total1 + total2 + total3) * 0.07);
     displayItemQuantities(coffeeCups, cheesecakeSlices, coffeeBeansAmount);
     displayTotalCoffeeSold(totalCoffeeSold);
     displayTotalMoneyMade(totalMoneyMade);
+    displaySalesData();
 }
 
 void displayItemQuantities(int coffeeCups, int cheesecakeSlices, double coffeeBeansAmount) {
@@ -126,6 +133,7 @@ void displayTotalMoneyMade(double totalMoney) {
     cout << "Total Money Made: $" << fixed << setprecision(2) << totalMoney << "\n";
     cout << "-------------------------\n";
 }
+
 int second_menu(int choice2) {
     if (choice2 == 1) {
         int correctPassword = MANAGER_PASSWORD;
@@ -158,101 +166,157 @@ int second_menu(int choice2) {
         cout << "An associate is headed your way. Thank you.\n";
     } else {
         return 0;
-}}
-
+    }
+    return 0;
+}
 
 double sellCoffee(int& coffeeCups) {
-    int cf;
+    int quantity;
     double total;
-    string tempChoice;
-    string size1;
+    string size;
 
-    cout << "  You Have selected To buy Coffee  \n";
-    cout << "\tWhat size of Coffee would you like to choose?\n";
-    cout << "Please enter L/l (12oz) for Large or S/s(9oz) for small : ";
-    cin >> size1;
+    cout << "  You have selected to buy Coffee\n";
+    cout << "What size of Coffee would you like to choose?\n";
+    cout << "Please enter 'L' for Large (12oz) or 'S' for Small (9oz): ";
+    cin >> size;
 
     do {
-        if (size1 == "L" || size1 == "l") {
-            cout << "Please enter the quantity you would like to buy : ";
-            cin >> cf;
-            cout << endl;
-            coffeeCups += cf; // Increment the quantity of coffee cups sold
-            total = cf * COFFEE_PRICE;
-
-            cout << "Would you like to purchase more Coffee? (Enter Y/y for y or enter any key for no) :";
-            cin >> tempChoice;
-            cout << endl;
-        } else if (size1 == "S" || size1 == "s") {
-            cout << "Please enter the quantity you would like to buy : ";
-            cin >> cf;
-            coffeeCups += cf; // Increment the quantity of coffee cups sold
-            total = cf * COFFEE_PRICE;
-            cout << "Would you like to purchase more Coffee? (Enter Y/y for y or enter any key for no) :";
-            cin >> tempChoice;
-            cout << endl;
+        if (size == "L" || size == "l") {
+            cout << "Please enter the quantity you would like to buy: ";
+            cin >> quantity;
+            coffeeCups += quantity;
+            total = quantity * LARGE_COFFEE_PRICE;
+        } else if (size == "S" || size == "s") {
+            cout << "Please enter the quantity you would like to buy: ";
+            cin >> quantity;
+            coffeeCups += quantity;
+            total = quantity * COFFEE_PRICE;
         } else {
-            cout << "Invalid Size Please Try Again!";
-            cout << "Would you like to purchase more Coffee? (Enter Y/y for y or enter any key for no) :";
-            cin >> tempChoice;
-            cout << endl;
+            cout << "Invalid size. Please try again!\n";
+            continue;
         }
-    } while (tempChoice == "Y" || tempChoice == "y");
 
-    cout << "You have purchased " << cf << " cup(s) of coffee. Your total cost is $" << total << endl;
+        cout << "You have purchased " << quantity << " cup(s) of coffee. Your total cost is $" << fixed << setprecision(2) << total << endl;
+
+        char choice;
+        cout << "Would you like to purchase more Coffee? (Enter 'Y' for yes or any other key for no): ";
+        cin >> choice;
+
+        if (tolower(choice) != 'y') {
+            break;
+        }
+
+        cout << "\nWhat size of Coffee would you like to choose?\n";
+        cout << "Please enter 'L' for Large (12oz) or 'S' for Small (9oz): ";
+        cin >> size;
+
+    } while (true);
+
     return total;
 }
 
 double sellCheesecake(int& cheesecakeSlices) {
-    int qty1;
+    int quantity;
     double total;
-    string tempChoice;
 
     do {
-        cout << " You Have selected To buy Cheesecake \n";
-        cout << "Cheesecake is $2.00 each \n";
-        cout << "Please enter the quantity you would like to buy : ";
-        cin >> qty1;
-        cheesecakeSlices += qty1; // Increment the quantity of cheesecake slices sold
-        total = qty1 * CHEESECAKE_PRICE;
-        cout << "You have purchased " << qty1 << " Cheesecake(s). Your total cost is $" << total << endl;
+        cout << "You have selected to buy Cheesecake\n";
+        cout << "Cheesecake is $3.00 each\n";
+        cout << "Please enter the quantity you would like to buy: ";
+        cin >> quantity;
+        cheesecakeSlices += quantity;
+        total = quantity * CHEESECAKE_PRICE;
 
-        cout << "Would you like to purchase more Cheesecake? (Enter Y/y for y or enter any key for no) :";
-        cin >> tempChoice;
-        cout << endl;
+        cout << "You have purchased " << quantity << " Cheesecake(s). Your total cost is $" << fixed << setprecision(2) << total << endl;
 
-    } while (tempChoice == "Y" || tempChoice == "y");
+        char choice;
+        cout << "Would you like to purchase more Cheesecake? (Enter 'Y' for yes or any other key for no): ";
+        cin >> choice;
+
+        if (tolower(choice) != 'y') {
+            break;
+        }
+
+    } while (true);
 
     return total;
 }
 
 double sellCoffeeBeans(double& coffeeBeansAmount) {
-    int qty2;
+    int quantity;
     double total;
-    string tempChoice;
 
     do {
-        cout << " You Have selected To buy Coffee Beans \n";
-        cout << "Coffee Beans are $5.00 per ounce \n";
-        cout << "Please enter the quantity you would like to buy : ";
-        cin >> qty2;
-        coffeeBeansAmount += qty2; // Accumulate the amount of coffee beans sold
-        total = qty2 * COFFEE_BEANS_PRICE;
-        cout << "You have purchased " << qty2 << " pounds of Coffee Beans. Your total cost is $" << total << endl;
+        cout << "You have selected to buy Coffee Beans\n";
+        cout << "Coffee Beans are $0.60 per ounce\n";
+        cout << "Please enter the quantity you would like to buy: ";
+        cin >> quantity;
+        coffeeBeansAmount += quantity;
+        total = quantity * COFFEE_BEANS_PRICE;
 
-        cout << "Would you like to purchase more Coffee Beans? (Enter Y/y for y or enter any key for no) :";
-        cin >> tempChoice;
-        cout << endl;
+        cout << "You have purchased " << quantity << " ounce(s) of Coffee Beans. Your total cost is $" << fixed << setprecision(2) << total << endl;
 
-    } while (tempChoice == "Y" || tempChoice == "y");
+        char choice;
+        cout << "Would you like to purchase more Coffee Beans? (Enter 'Y' for yes or any other key for no): ";
+        cin >> choice;
+
+        if (tolower(choice) != 'y') {
+            break;
+        }
+
+    } while (true);
 
     return total;
 }
+
 void orderReview(double total1, double total2, double total3) {
     cout << "*****************************************\n";
     cout << "* Order Review: \n";
-    cout << "* Coffee: $" << total1 << "\n";
-    cout << "* Cheesecake: $" << total2 << "\n";
-    cout << "* Coffee Beans: $" << total3 << "\n";
+    cout << "* Coffee: $" << fixed << setprecision(2) << total1 << "\n";
+    cout << "* Cheesecake: $" << fixed << setprecision(2) << total2 << "\n";
+    cout << "* Coffee Beans: $" << fixed << setprecision(2) << total3 << "\n";
     cout << "*****************************************\n";
+}
+
+void recordTransaction(const string& items, int quantity, double total) {
+    // Increment the transaction ID counter
+    int transactionID = ++transactionIDCounter;
+
+    time_t now = time(0);
+    tm* localTime = localtime(&now);
+    char dateTime[80];
+    strftime(dateTime, 80, "%Y-%m-%d %H:%M:%S", localTime);
+
+    // Open the file in append mode
+    ofstream transactionFile("transaction_history.txt", ios::app);
+    if (transactionFile.is_open()) {
+        // Record transaction details in the file
+        transactionFile << "Transaction ID: " << transactionID << endl;
+        transactionFile << "Items Bought: " << items << endl;
+        transactionFile << "Quantity: " << quantity << endl;
+        transactionFile << "Transaction Total: $" << fixed << setprecision(2) << total << endl;
+        transactionFile << "Date and Time: " << dateTime << endl;
+        transactionFile << "--------------------------------------------\n";
+
+        cout << "Transaction recorded successfully.\n";
+        transactionFile.close();
+    } else {
+        cerr << "Error opening transaction file.\n";
+    }
+}
+
+void displaySalesData() {
+    cout << "\nSales Data from Transaction History:\n";
+
+    // Open the transaction details file
+    ifstream transactionFile("transaction_history.txt");
+    if (transactionFile.is_open()) {
+        string line;
+        while (getline(transactionFile, line)) {
+            cout << line << endl;
+        }
+        transactionFile.close();
+    } else {
+        cerr << "Error opening transaction file.\n";
+    }
 }
